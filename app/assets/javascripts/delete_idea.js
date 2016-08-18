8b8de55385169ea58a52ed8e71ae00d6122ab647
Parker P <exxcaelum@gmail.com>
2016-08-18 13:51:48 -0600

    removing psuedo code

--- app/assets/javascripts/delete_idea.js
+++ app/assets/javascripts/delete_idea.js
@@ -1,16 +1,3 @@
-// function deleteIdea() {
-//   $(document).on('click', '.deletePost', function(){
-//     var postId = $(this).parent().data('postId');
-//     $.ajax({
-//       method: "DELETE",
-//       url: "/api/v1/ideas/" + postId,
-//       data: {id: postId},
-//       success: function(deletedPost){
-//         $(".post[data-post-id=" + postId + "]").remove();
-//       }
-//     });
-//   });
-// }
 function deleteIdea() {
   $(document).on('click', '.deletePost', function(){
     var postId = $(this).parent().data('postId');
