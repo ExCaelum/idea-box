19fc6473461f3c7f237c14fda7c2ea440362a8ca
Parker P <exxcaelum@gmail.com>
2016-08-17 22:18:06 -0600

    creation endpoint in API created

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,5 +3,20 @@ class Api::V1::IdeasController < ApplicationController
   def index
     render json: Idea.all
   end
-  
+
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      render json: idea
+    else
+      render json: {error: "invalid Data"}
+    end
+  end
+
+  private
+
+  def idea_params
+    params.permit(:title, :body)
+  end
+
 end
