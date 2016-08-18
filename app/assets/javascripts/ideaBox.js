e9a55d2c2af46ce3680537d2533fb8141025997a
Parker P <exxcaelum@gmail.com>
2016-08-17 22:28:03 -0600

    user is able to create ideas

--- app/assets/javascripts/ideaBox.js
+++ app/assets/javascripts/ideaBox.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
   buildIdeas()
-
-
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/v1/ideas",
-  //   dataType: "JSON",
-  //   success: function(response){
-  //     response.map(function(idea){
-  //       $('#ideas').append(
-  //         buildIdea(idea)
-  //       )
-  //     })
-  //   }
-  // })
-
+  createIdea()
 })
